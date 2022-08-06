95b824e620fd88ce3a7cf29535b3ea782e324da0
Gidwani-Nitesh <19egjcs158@gitjaipur.com>
2022-08-06 14:02:03 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+
+    return (
+        <div className='nav-bar-container'>
+            <span className='nav-title'>To Do List </span>
+        </div>
+    );
+}
+export default Nav;
